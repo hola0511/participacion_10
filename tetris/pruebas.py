5978add3c65911d4b4a360f317425f9db713aac0
samuel espinosa arias <76191247+hola0511@users.noreply.github.com>
2023-11-02 12:47:55 -0500

    Update pruebas.py

--- tetris/pruebas.py
+++ tetris/pruebas.py
@@ -5,21 +5,21 @@ from tetris import Tetromino
 class TestTetromino(unittest.TestCase):
     def setUp(self):
         self.T_shape = [[False, False, False, False], [False, True, True, True], [False, False, True, False], [False, False, False, False]]
-        self.Z_shape = [[False, False, False, False], [False, False, True, True], [False, True, True, False], [False, False, False, False]]
+        self.S_shape = [[False, False, False, False], [False, False, True, True], [False, True, True, False], [False, False, False, False]]
         self.T_tetromino = Tetromino(self.T_shape)
-        self.Z_tetromino = Tetromino(self.Z_shape)
+        self.S_tetromino = Tetromino(self.S_shape)
 
     def test_rotation(self):
-        self.T_tetromino.rotate_clockwise()
-        self.assertEqual(self.T_tetromino.shape, [[False, False, False, False], [False, False, True, False], [False, True, True, False], [False, False, True, False]])
+        self.T_tetromino.rotar()
+        self.assertEqual(self.T_tetromino.shape, [[False, False, True, False], [False, True, True, False], [False, False, True, False], [False, False, False, False]])
 
     def test_equality(self):
         self.assertTrue(self.T_tetromino == self.T_tetromino)
 
     def test_similarity(self):
-        self.assertTrue(self.T_tetromino.check_similarity(Tetromino(self.T_shape)))
-        self.assertTrue(self.T_tetromino.check_similarity(Tetromino(self.T_shape)))
-        self.assertFalse(self.T_tetromino.check_similarity(self.Z_tetromino))
+        self.assertTrue(self.T_tetromino.similar(Tetromino(self.T_shape)))
+        self.assertTrue(self.T_tetromino.similar(Tetromino(self.T_shape)))
+        self.assertFalse(self.T_tetromino.similar(self.S_tetromino))
 
 
 if __name__ == '__main__':
